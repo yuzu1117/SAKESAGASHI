875d524e587ace693d40f8a051cd0d7ab97d9a0f
Naoko Tashiro <108598161+yuzu1117@users.noreply.github.com>
2022-09-21 10:25:59 +0000

    commit

--- app/controllers/user/items_controller.rb
+++ app/controllers/user/items_controller.rb
@@ -4,11 +4,14 @@ class User::ItemsController < ApplicationController
     @items = Item.search(params[:keyword])
     @keyword = params[:keyword]
     @genres = Genre.all
+    #ランキングから探すメソッド
+    @ranks = Item.find(Review.group(:item_id).order('avg(star) desc').pluck(:item_id))
   end
 
   def index
     keyword = params[:keyword]
     @items = Item.where("name LIKE(?)", "%#{keyword}%")
+    #ジャンル検索
     if params[:genre_id]
        @genre = Genre.find(params[:genre_id])
        @items = @genre.items.all
@@ -18,8 +21,9 @@ class User::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @reviews = @item.reviews.all
+    #星の平均値を出すメソッド
     @average = @item.reviews.average(:star)
-    #味の平均値
+    #味の平均値を出すメソッド
     @ave_sharpness = @reviews.average(:sharpness)
     @ave_rich = @reviews.average(:rich)
     @ave_sourness = @reviews.average(:sourness)
